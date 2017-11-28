40b586eda34220cff3c2991cd758b2e2c57e3be2
Mojotatan <tatan42@gmail.com>
2017-11-28 15:47:00 -0600

    incorporated react

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const db = require('./db').db
 
@@ -22,7 +23,7 @@ db.sync()
     .use('/api', require('./api/api'))
 
     .get('*', (req, res) => {
-      res.sendFile('/public/index.html')
+      res.sendFile(path.resolve(__dirname, 'public/index.html'))
     })
 })
 .catch(err => console.error(err))
