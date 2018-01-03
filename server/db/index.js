848d7455fdb9d57b731ce92b30bda1f3939835f0
Mojotatan <tatan42@gmail.com>
2018-01-03 16:35:17 -0600

    whatever

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 // Define db
 const Sequelize = require('sequelize')
-const dbUrl = process.env.DATABASE_URL || 'postgres://localhost:5432/aci'
+const dbUrl = process.env.DATABASE_URL || 'mysql://root:root@localhost:8889/aci' /*'postgres://localhost:5432/aci'*/
 let db = new Sequelize(dbUrl, {logging: false})
 
 // Import tables
