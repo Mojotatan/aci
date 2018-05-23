b50dec0db353578df0f5bb772aa2a16db19a591d
Mojotatan <tatan42@gmail.com>
2018-05-23 16:07:56 -0500

    no one knows

--- browser/containers/Buyout.js
+++ browser/containers/Buyout.js
@@ -241,8 +241,8 @@ class BuyoutContainer extends React.Component {
 
     axios.post('/api/mail', {
       token: this.props.token,
-      // to: this.state.rep.email,
-      to: 'tatan42@gmail.com',
+      to: this.state.rep.email,
+      // to: 'tatan42@gmail.com',
       cc: this.state.mailCC.split(', '),
       subject: this.state.mailSubject,
       html: this.state.mailBody
