50a5b3342d714563ada0a6dd492821723aa030ae
Mojotatan <tatan42@gmail.com>
2018-06-18 15:36:18 -0500

    formatting and autofill

--- browser/components/BuyoutCalcs.js
+++ browser/components/BuyoutCalcs.js
@@ -17,50 +17,55 @@ export default ({
       <div className="app-bg col-sm-12 calcs">
         <h3>Lease Details</h3>
         <div className="row">
-          <div className="col-sm-4">
+          {/* <div className="col-sm-4">
             <div className="field-label">
               <label>Lessee Name</label>
             </div>
             <div className="field-box">
-              <div></div>
+              <p>{values.customer.name}</p>
             </div>
-          </div>
-          <div className="col-sm-4">
+          </div> */}
+          <div className="col-sm-3">
             <div className="field-label">
               <label>Lease Company</label>
             </div>
             <div className="field-box">
-              <input name="leaseCompany" value={values.calcs.leaseCompany || ''} onChange={handleCalcs} />
+              <datalist id="leaseCompanies">
+                <option value="EverBank"></option>
+                <option value="DLL"></option>
+                <option value="Wells"></option>
+                <option value="USB"></option>
+                <option value="CIT"></option>
+                <option value="Marlin"></option>
+                <option value="Balboa"></option>
+                <option value="EMR"></option>
+                <option value="Leaf"></option>
+                <option value="Great America"></option>
+                <option value="PNC"></option>
+              </datalist>
+              <input list="leaseCompanies" name="leaseCompany" value={values.calcs.leaseCompany || ''} onChange={handleCalcs} autoComplete="new-password" />
             </div>
           </div>
-          <div className="col-sm-4">
+          <div className="col-sm-3">
             <div className="field-label">
               <label>Lease Number</label>
             </div>
             <div className="field-box">
-              <input name="leaseNumber" value={values.calcs.leaseNumber || ''} onChange={handleCalcs} />
+              <input name="leaseNumber" value={values.calcs.leaseNumber || ''} onChange={handleCalcs} autoComplete="new-password" />
             </div>
           </div>
         </div>
 
-        <div className="row">
-          <div className="col-sm-6">
+        {/* <div className="row">
+          <div className="col-sm-12">
             <div className="field-label">
               <label>Address</label>
             </div>
             <div className="field-box">
-              <div></div>
+              <p>{values.customer.address}</p>
             </div>
           </div>
-          <div className="col-sm-6">
-            <div className="field-label">
-              <label>City, State, Zip</label>
-            </div>
-            <div className="field-box">
-              <div></div>
-            </div>
-          </div>
-        </div>
+        </div> */}
 
         <div className="row">
           <div className="col-sm-3">
@@ -68,7 +73,7 @@ export default ({
               <label>Lease Start Date</label>
             </div>
             <div className="field-box">
-              <input name="startDate" value={values.calcs.startDate || ''} onChange={handleCalcs} />
+              <input name="startDate" value={values.calcs.startDate || ''} onChange={handleCalcs} autoComplete="new-password" />
             </div>
           </div>
           <div className="col-sm-3 col-sm-offset-3">
@@ -76,7 +81,7 @@ export default ({
               <label>Original Term</label>
             </div>
             <div className="field-box">
-              <input name="originalTerm" value={values.calcs.originalTerm || ''} onChange={handleCalcs} />
+              <input name="originalTerm" value={values.calcs.originalTerm || ''} onChange={handleCalcs} autoComplete="new-password" />
             </div>
           </div>
         </div>
@@ -87,7 +92,7 @@ export default ({
               <label>Lease Plan End Date</label>
             </div>
             <div className="field-box">
-              <input name="endDate" value={values.calcs.endDate || ''} onChange={handleCalcs} />
+              <input name="endDate" value={values.calcs.endDate || ''} onChange={handleCalcs} autoComplete="new-password" />
             </div>
           </div>
           <div className="col-sm-3 col-sm-offset-3">
@@ -95,7 +100,7 @@ export default ({
               <label>Remaining Term</label>
             </div>
             <div className="field-box">
-              <input name="remainingTerm" value={values.calcs.remainingTerm || ''} onChange={handleCalcs} />
+              <input name="remainingTerm" value={values.calcs.remainingTerm || ''} onChange={handleCalcs} autoComplete="new-password" />
             </div>
           </div>
         </div>
@@ -106,7 +111,7 @@ export default ({
               <label>Current Equipment Payment</label>
             </div>
             <div className="field-box monetary">
-              <input name="currentEquipmentPayment" value={values.calcs.currentEquipmentPayment || ''} onChange={handleCalcs} />
+              <input name="currentEquipmentPayment" value={values.calcs.currentEquipmentPayment || ''} onChange={handleCalcs} autoComplete="new-password" />
             </div>
           </div>
         </div>
@@ -116,7 +121,7 @@ export default ({
               <label>Current Service/MA Payment</label>
             </div>
             <div className="field-box monetary">
-              <input name="currentServicePayment" value={values.calcs.currentServicePayment || ''} onChange={handleCalcs} />
+              <input name="currentServicePayment" value={values.calcs.currentServicePayment || ''} onChange={handleCalcs} autoComplete="new-password" />
             </div>
           </div>
         </div>
@@ -126,7 +131,7 @@ export default ({
               <label>Pass Through Service</label>
             </div>
             <div className="field-box monetary">
-              <input name="passThroughService" value={values.calcs.passThroughService || ''} onChange={handleCalcs} />
+              <input name="passThroughService" value={values.calcs.passThroughService || ''} onChange={handleCalcs} autoComplete="new-password" />
             </div>
           </div>
           <div className="col-sm-3 col-sm-offset-3">
@@ -149,7 +154,7 @@ export default ({
               <label>Upgrade to Keep</label>
             </div>
             <div className="field-box monetary">
-              <input name="companyUtk" value={values.calcs.companyUtk || ''} onChange={handleCalcs} />
+              <input name="companyUtk" value={values.calcs.companyUtk || ''} onChange={handleCalcs} autoComplete="new-password" />
             </div>
           </div>
           <div className="col-sm-3 col-sm-offset-3">
@@ -157,7 +162,7 @@ export default ({
               <label>Buyout to Keep</label>
             </div>
             <div className="field-box monetary">
-              <input name="companyBtk" value={values.calcs.companyBtk || ''} onChange={handleCalcs} />
+              <input name="companyBtk" value={values.calcs.companyBtk || ''} onChange={handleCalcs} autoComplete="new-password" />
             </div>
           </div>
         </div>
@@ -168,7 +173,7 @@ export default ({
               <label>Upgrade to Return</label>
             </div>
             <div className="field-box monetary">
-              <input name="companyUtr" value={values.calcs.companyUtr || ''} onChange={handleCalcs} />
+              <input name="companyUtr" value={values.calcs.companyUtr || ''} onChange={handleCalcs} autoComplete="new-password" />
             </div>
           </div>
           <div className="col-sm-3 col-sm-offset-3">
@@ -176,7 +181,7 @@ export default ({
               <label>Buyout to Return</label>
             </div>
             <div className="field-box monetary">
-              <input name="companyBtr" value={values.calcs.companyBtr || ''} onChange={handleCalcs} />
+              <input name="companyBtr" value={values.calcs.companyBtr || ''} onChange={handleCalcs} autoComplete="new-password" />
             </div>
           </div>
         </div>
@@ -187,7 +192,7 @@ export default ({
               <label>%</label>
             </div>
             <div className="field-box">
-              <input name="percentage" value={values.calcs.percentage || ''} onChange={handleCalcs} />
+              <input name="percentage" value={values.calcs.percentage || ''} onChange={handleCalcs} autoComplete="new-password" />
             </div>
           </div>
           <div className="col-sm-3">
@@ -215,7 +220,7 @@ export default ({
               <label>MISC (SMUA)</label>
             </div>
             <div className="field-box monetary">
-              <input name="smua" value={values.calcs.smua || ''} onChange={handleCalcs} />
+              <input name="smua" value={values.calcs.smua || ''} onChange={handleCalcs} autoComplete="new-password" />
             </div>
           </div>
         </div>
@@ -289,7 +294,7 @@ export default ({
               <label>Buyout to Keep</label>
             </div>
             <div className="field-box monetary">
-              <input name="customerBtk" value={values.calcs.customerBtk || ''} onChange={handleCalcs} />
+              <input name="customerBtk" value={values.calcs.customerBtk || ''} onChange={handleCalcs} autoComplete="new-password" />
             </div>
           </div>
         </div>
@@ -299,7 +304,7 @@ export default ({
               <label>Buyout to Return</label>
             </div>
             <div className="field-box monetary">
-              <input name="customerBtr" value={values.calcs.customerBtr || ''} onChange={handleCalcs} />
+              <input name="customerBtr" value={values.calcs.customerBtr || ''} onChange={handleCalcs} autoComplete="new-password" />
             </div>
           </div>
         </div>
@@ -317,7 +322,7 @@ export default ({
             Upfront Tax
           </div>
           <div className="col-sm-2">
-            <input className="sm-input rightput" name="taxRate" value={values.calcs.taxRate || ''} onChange={handleCalcs} />
+            <input className="sm-input rightput" name="taxRate" value={values.calcs.taxRate || ''} onChange={handleCalcs} autoComplete="new-password" />
             <span>% Tax</span>
           </div>
           <div className="col-sm-2">
@@ -332,7 +337,7 @@ export default ({
             <div>${sum(values.calcs.currentEquipmentPayment)}</div>            
           </div>
           <div className="col-sm-2">
-            $<input className="sm-input" name="upfrontTax" value={values.calcs.upfrontTax || ''} onChange={handleCalcs} />
+            $<input className="sm-input" name="upfrontTax" value={values.calcs.upfrontTax || ''} onChange={handleCalcs} autoComplete="new-password" />
           </div>
           <div className="col-sm-2">
             <div>${round(sum(
@@ -371,7 +376,7 @@ export default ({
             <div>Fuel/Freight</div>
           </div>
           <div className="col-sm-2">
-            $<input className="sm-input" name="fuelFreight" value={values.calcs.fuelFreight || ''} onChange={handleCalcs} />
+            $<input className="sm-input" name="fuelFreight" value={values.calcs.fuelFreight || ''} onChange={handleCalcs} autoComplete="new-password" />
           </div>
           <div className="col-sm-2">
             <div></div>
@@ -388,7 +393,7 @@ export default ({
             <div>Late Charges</div>
           </div>
           <div className="col-sm-2">
-            $<input className="sm-input" name="lateCharges" value={values.calcs.lateCharges || ''} onChange={handleCalcs} />
+            $<input className="sm-input" name="lateCharges" value={values.calcs.lateCharges || ''} onChange={handleCalcs} autoComplete="new-password" />
           </div>
           <div className="col-sm-2">
             <div></div>
@@ -405,7 +410,7 @@ export default ({
             <div>Misc. Items *See Notes</div>
           </div>
           <div className="col-sm-2">
-            $<input className="sm-input" name="miscItems" value={values.calcs.miscItems || ''} onChange={handleCalcs} />
+            $<input className="sm-input" name="miscItems" value={values.calcs.miscItems || ''} onChange={handleCalcs} autoComplete="new-password" />
           </div>
           <div className="col-sm-2">
             <div></div>
@@ -541,7 +546,11 @@ export default ({
         <div className="row">
           <div className="col-sm-12">
             <div className="field-desc">
-              <textarea></textarea>
+              <textarea
+              name="notes"
+              value={values.calcs.notes || ''}
+              onChange={handleCalcs} autoComplete="new-password"
+              />
             </div>
           </div>
         </div>
